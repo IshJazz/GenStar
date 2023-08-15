78907fc645ea6048fb6294803f25cfc58dd6baec
Ishan Dikshit <ishandikshit67@gmail.com>
2023-08-15 12:25:50 +0530

    final commit

---
+++ server/index.js
@@ -0,0 +1,31 @@
+import express from "express";
+import * as dotenv from "dotenv";
+import cors from "cors";
+import connectDB from "./mongodb/connect.js";
+import postRoutes from "./routes/postRoutes.js";
+import genstarRoutes from "./routes/genstarRoutes.js";
+
+dotenv.config();
+
+const app = express();
+app.use(cors());
+app.use(express.json({ limit: "50mb" }));
+app.use("/api/v1/post", postRoutes);
+app.use("/api/v1/genstar", genstarRoutes);
+
+app.get("/", async (req, res) => {
+  res.send("Hello from GenStar");
+});
+
+const startServer = async () => {
+  try {
+    connectDB(process.env.MONGODB_URL);
+    app.listen(8080, () =>
+      console.log("Server has started on port http://localhost:8080")
+    );
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+startServer();
